65f9de883a2db06bf738fa9daab9df58f72ce7d2
Emil-Petrov <emil.petrov95@gmail.com>
2015-05-04 14:32:14 +0300

    Cell class didn't push

--- minesweeper/MinesweeperCell.cs
+++ minesweeper/MinesweeperCell.cs
@@ -2,50 +2,20 @@
 {
     class MinesweeperCell
     {
-        private char val = '?';
-        private bool revealed = false;
-        //constructors
-        public MinesweeperCell(char val, bool revealed)
-        {
-            this.Value = val;
-            this.Revealed = revealed;
-        }
         public MinesweeperCell()
         {
             this.Value = ' ';
             this.Revealed = false;
         }
-        //properties
         public char VisibleValue
         {
             get
             {
-                var result = revealed == false ? '?' : val;
+                return this.Revealed ? '?' : this.Value;
+            }
+        }
+        public char Value { get; set; }
 
-                return result;
-            }
-        }
-        public char Value
-        { 
-            get
-            {
-                return val;
-            }
-            set
-            {
-                val = value;
-            }
-        }
-        public bool Revealed 
-        { 
-            get
-            {
-                return revealed;
-            } 
-            set
-            {
-                revealed = value;
-            }
-        }
+        public bool Revealed { get; set; }
     }
 }
