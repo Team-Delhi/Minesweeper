9edd461c0e1f975b8e8f1d700171f269787c73b1
Emil-Petrov <emil.petrov95@gmail.com>
2015-05-04 13:50:02 +0300

    Rearranged code

--- minesweeper/MinesweeperGrid.cs
+++ minesweeper/MinesweeperGrid.cs
@@ -31,140 +31,15 @@ namespace MinesweeperProject
             }
         }
 
-        public void FillGrid(MinesweeperCell[,] minesGrid, int rows, int columns)
-        {
-            for (int row = 0; row < Rows; row++)
-            {
-                for (int column = 0; column < Columns; column++)
-                {
-                    minesGrid[row, column] = new MinesweeperCell();
-                }
-            }
-        }
-
-        public bool IsCellOnBoard(int row, int column)
-        {
-            bool onBoard = (row >= 0 && row < Rows) && (column >= 0 && column < Columns);
-            return onBoard;
-        }
-
-        private void SetCellValue(int row, int column, char value)
-        {
-            if (!IsCellOnBoard(row, column))
-            {
-                throw new InvalidCellException();
-            }
-
-            Grid[row, column].Value = value;
-        }
-
-        private char GetCellValue(int row, int column)
-        {
-            if (!IsCellOnBoard(row, column))
-            {
-                throw new InvalidCellException();
-            }
-
-            char result = grid[row, column].Value;
-            return result;
-        }
-
-        public char RevealCell(int row, int column)
-        {
-            if ((!IsCellOnBoard(row, column)) || grid[row, column].Revealed)
-            {
-                throw new InvalidCellException();
-            }
-
-            grid[row, column].Reveal();
-
-            if (grid[row, column].Value != '*')
-            {
-                int neighbourMinesCount = GetNeighbourMinesCount(row, column);
-                SetCellValue(row, column, neighbourMinesCount.ToString()[0]);
-            }
-            return grid[row, column].Value;
-        }
-
-        public int GetNeighbourMinesCount(int row, int column)
-        {
-            if (!IsCellOnBoard(row, column))
-            {
-                throw new InvalidCellException();                
-            }
-
-            //restrict neigbour cell area
-            int previousRow = (row - 1) < 0 ? row : row - 1;
-            int nextRow = (row + 1) >= Rows ? row : row + 1;
-            int previousColumn = (column - 1) < 0 ? column : column - 1;
-            int nextColumn = (column + 1) >= Columns ? column : column + 1; ;
-
-            int count = 0;
-            for (int i = previousRow; i <= nextRow; i++)
-            {
-                for (int j = previousColumn; j <= nextColumn; j++)
-                {
-                    if (grid[i, j].Value == '*')
-                    {
-                        count++;
-                    }
-                }
-            }
-            return count;
-        }
-
-        private void PlaceMines()
-        {
-            List<int[]> coordinates = GetRandomCoorindates(this.MinesCount);
-            coordinates.ForEach(coordinate => SetCellValue(coordinate[0], coordinate[1], '*'));
-        }
-
-        private List<int[]> GetRandomCoorindates(int minesCount)
-        {
-            List<int[]>  coordinates = new List<int[]>(minesCount);
-            Random rnd = new Random();
-            while (coordinates.Count < minesCount)
-            {
-                int row = rnd.Next(Rows);
-                int col = rnd.Next(Columns);
-                int[] coordinate = new int[] { row, col };
-                if (!CoordinateExists(coordinates, coordinate))
-                {
-                    coordinates.Add(new[] { row, col });
-                }
-            }
-
-            return coordinates;
-        }
-
-        private bool CoordinateExists(List<int[]> coordinates, int[] coordinate)
-        {
-            return coordinates.Select(coord => coord.SequenceEqual(coordinate)).Any(isContained => isContained);
-        }
-
-        
         public void RestartBoard()
         {
             RestartGrid();
             PlaceMines();
         }
 
-        public void RestartGrid()
+        private void RestartGrid()
         {
             this.Grid = new MinesweeperCell[this.Rows, this.Columns];
-        }
-
-        public int RevealedCount()
-        {
-            int count = 0;
-            foreach (MinesweeperCell field in this.Grid)
-            {
-                if (field.Revealed)
-                {
-                    count++;
-                }
-            }
-            return count;
         }
 
         public void RevealMines()
@@ -189,6 +64,107 @@ namespace MinesweeperProject
                 }
             }
         }
+
+        public void FillGrid(MinesweeperCell[,] minesGrid, int rows, int columns)
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int column = 0; column < Columns; column++)
+                {
+                    minesGrid[row, column] = new MinesweeperCell();
+                }
+            }
+        }
+
+        public bool IsCellOnBoard(int row, int column)
+        {
+            bool onBoard = (row >= 0 && row < Rows) && (column >= 0 && column < Columns);
+            return onBoard;
+        }
+
+        public char RevealCell(int row, int column)
+        {
+            if ((!IsCellOnBoard(row, column)) || grid[row, column].Revealed)
+            {
+                throw new InvalidCellException();
+            }
+
+            grid[row, column].Reveal();
+
+            if (grid[row, column].Value != '*')
+            {
+                int neighbourMinesCount = GetNeighbourMinesCount(row, column);
+                SetCellValue(row, column, neighbourMinesCount.ToString()[0]);
+            }
+            return grid[row, column].Value;
+        }
+
+        public int GetNeighbourMinesCount(int row, int column)
+        {
+            if (!IsCellOnBoard(row, column))
+            {
+                throw new InvalidCellException();
+            }
+
+            //restrict neigbour cell area
+            int previousRow = (row - 1) < 0 ? row : row - 1;
+            int nextRow = (row + 1) >= Rows ? row : row + 1;
+            int previousColumn = (column - 1) < 0 ? column : column - 1;
+            int nextColumn = (column + 1) >= Columns ? column : column + 1; ;
+
+            int count = 0;
+            for (int i = previousRow; i <= nextRow; i++)
+            {
+                for (int j = previousColumn; j <= nextColumn; j++)
+                {
+                    if (grid[i, j].Value == '*')
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private void SetCellValue(int row, int column, char value)
+        {
+            if (!IsCellOnBoard(row, column))
+            {
+                throw new InvalidCellException();
+            }
+
+            Grid[row, column].Value = value;
+        }
+
+        private void PlaceMines()
+        {
+            List<int[]> coordinates = GetRandomCoorindates(this.MinesCount);
+            coordinates.ForEach(coordinate => SetCellValue(coordinate[0], coordinate[1], '*'));
+        }
+
+        private List<int[]> GetRandomCoorindates(int minesCount)
+        {
+            List<int[]> coordinates = new List<int[]>(minesCount);
+            Random rnd = new Random();
+            while (coordinates.Count < minesCount)
+            {
+                int row = rnd.Next(Rows);
+                int col = rnd.Next(Columns);
+                int[] coordinate = new int[] { row, col };
+                if (!CoordinateExists(coordinates, coordinate))
+                {
+                    coordinates.Add(new[] { row, col });
+                }
+            }
+
+            return coordinates;
+        }
+
+        private bool CoordinateExists(List<int[]> coordinates, int[] coordinate)
+        {
+            return coordinates.Select(coord => coord.SequenceEqual(coordinate)).Any(isContained => isContained);
+        }
+
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
